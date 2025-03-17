6c4cd55f550659a0978613b4d9bfb75f16e3b9f1
unknown <pradeephs17@gmail.com>
2025-03-17 16:43:05 +0530

    inter

--- emp_min.sql
+++ emp_min.sql
@@ -1,6 +1,6 @@
 select * from(select *,min(sal)over(partition by deptno)as rnk from empnew)m
-
-
-intersect
+intersection
 select * from empnew
-intersect select * from empnew
+intersection
+select * from empnew
+
